f28b684cf6dd6831f45c63094d4b7232cf0fc385
Maxim Sekerjitsky <msekerjitsky@tula.co>
2017-12-26 16:55:21 +0300

    Minor changes.

--- src/main/java/com/mobcrush/wowza/FFMpegService.java
+++ src/main/java/com/mobcrush/wowza/FFMpegService.java
@@ -15,23 +15,21 @@ public class FFMpegService {
 
     public static void run(CompositeActionModel actionModel) {
 
-//        LOGGER.error("Path:" + System.getProperties().getProperty("user.home") + System.getProperties().getProperty("file.separator") + "despacito.mp4");
-
         try {
             FFmpeg ffmpeg = new FFmpeg();
             FFprobe ffprobe = new FFprobe();
             FFmpegBuilder builder = new FFmpegBuilder()
-                    .setVerbosity(FFmpegBuilder.Verbosity.DEBUG)
-                    .setInput("c:\\Users\\msekerjitsky\\despacito.mp4")
+                    .setInput(actionModel.getMasterStreamUrl())
                     .addInput(actionModel.getSlaveStreamUrl())
                     .setComplexFilter("[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]")
                     .addOutput(actionModel.getTargetStreamUrl())
-                    .addExtraArgs("-map", "[vid]")
-                    .setVideoCodec("libx264")
-                    .setConstantRateFactor(23)
-                    .setPreset("veryfast")
-                    .setFormat("flv")
-                    .done();
+                        .addExtraArgs("-map", "[vid]")
+                        .setVideoCodec("libx264")
+                        .setConstantRateFactor(23)
+                        .setPreset("veryfast")
+                        .setFormat("flv")
+                        .addExtraArgs("-shortest")
+                        .done();
 
             FFmpegJob job = new FFmpegExecutor(ffmpeg, ffprobe).createJob(builder, new FFMpegProgressListener());
             new Thread(() -> job.run()).start();
