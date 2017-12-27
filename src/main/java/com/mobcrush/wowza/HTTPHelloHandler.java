48f060f3bfbd8a25cf9cd421853d4d5cc1d1b054
Maxim Sekerjitsky <msekerjitsky@tula.co>
2017-12-27 16:16:43 +0300

    Add validation for stream names.

--- src/main/java/com/mobcrush/wowza/HTTPHelloHandler.java
+++ src/main/java/com/mobcrush/wowza/HTTPHelloHandler.java
@@ -7,13 +7,21 @@ import com.wowza.wms.http.IHTTPRequest;
 import com.wowza.wms.http.IHTTPResponse;
 import com.wowza.wms.logging.WMSLogger;
 import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.stream.MediaStreamMap;
 import com.wowza.wms.vhost.IVHost;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HTTPHelloHandler extends HTTPProvider2Base {
+
+    private static final String APPLICATION_NAME = "live";
+    private static final String APPLICATION_INSTANCE_NAME = "_definst_";
 
     private WMSLogger logger = WMSLoggerFactory.getLogger(this.getClass());
 
@@ -34,17 +42,21 @@ public class HTTPHelloHandler extends HTTPProvider2Base {
         }
 
         if (requestBody == null) {
-            response.setResponseCode(400);
+            response.setResponseCode(HttpStatus.SC_BAD_REQUEST);
             return;
         }
 
         CompositeActionModel model = parseRequestBody(requestBody);
         if (model == null) {
-            response.setResponseCode(400);
+            response.setResponseCode(HttpStatus.SC_BAD_REQUEST);
             return;
         }
 
-        response.setResponseCode(200);
+        if (!validateStreamNames(host, response, model)) {
+            return;
+        }
+
+        response.setResponseCode(HttpStatus.SC_OK);
         FFMpegService.run(model);
     }
 
@@ -61,14 +73,33 @@ public class HTTPHelloHandler extends HTTPProvider2Base {
         return result;
     }
 
-  private boolean validateRequest(IHTTPRequest request, IHTTPResponse response) {
-    boolean isValid = true;
+    private boolean validateRequest(IHTTPRequest request, IHTTPResponse response) {
+        boolean isValid = true;
 
-    if (!request.getMethod().equalsIgnoreCase("post")) {
-        isValid = false;
-        response.setResponseCode(405);
+        if (!HttpPost.METHOD_NAME.equalsIgnoreCase(request.getMethod())) {
+            isValid = false;
+            response.setResponseCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+        }
+
+        return isValid;
     }
 
-    return isValid;
-  }
+    private boolean validateStreamNames(IVHost host, IHTTPResponse response, CompositeActionModel model) {
+
+        MediaStreamMap streams = host.getApplication(APPLICATION_NAME).getAppInstance(APPLICATION_INSTANCE_NAME)
+                .getStreams();
+        List<String> streamsUrl = streams.getStreams().stream()
+                .map(stream -> stream.getClient().getUri() + "/" + stream.getName())
+                .collect(Collectors.toList());
+
+        if (!streamsUrl.contains(model.getMasterStreamUrl())
+                || !streamsUrl.contains(model.getSlaveStreamUrl())
+                || streamsUrl.contains(model.getTargetStreamUrl())
+        ) {
+            response.setResponseCode(HttpStatus.SC_BAD_REQUEST);
+            return false;
+        }
+
+        return true;
+    }
 }
